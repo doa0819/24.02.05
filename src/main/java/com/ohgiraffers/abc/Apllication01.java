53f44050b871bd58de672ca16d0bc5fc8ba7ab83
doa <leedoa0819@gmail.com>
2024-02-05 17:01:50 +0900

    [24.02.05]

---
+++ src/main/java/com/ohgiraffers/abc/Apllication01.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.abc;
+
+public class Apllication01 {
+
+    public static void main(String[] args) {
+
+        int y =5;
+        char c = 'A';
+
+        System.out.println(++c);
+
+
+        int x = 2;
+        System.out.println(y += 10 - x);
+        System.out.println('C'-c);
+
+        System.out.println(y >= 5 || x < 0 && x > 2);
+        System.out.println(y += 10 - x++);
+        System.out.println(x+=2);
+        System.out.println( !('A' <= c && c <='Z') );
+        System.out.println('C'-c);
+
+        int num = 888;
+        System.out.println(num/100*100+1);
+
+
+
+
+
+    }
+}
